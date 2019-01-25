b6637831517eb26340b3688fba2bcd379a6fd520
einar larsen <einarlarsen@outlook.com>
2019-01-25 20:45:53 +0100

    more comments

--- src/__init__.py
+++ src/__init__.py
@@ -5,14 +5,14 @@ import os
 config_file = "config.json"
 
 if not os.path.isfile(config_file):
+    # For testing purposes
     config_file = "../config.json"
 
 with open(config_file, "r") as cfg:
     config = json.load(cfg)
 
-# TODO: Validate config...
+# Validate config?...
 config = munch.munchify(config)
-
 
 from .filtering.filter_alerts import AlertFilter
 from .notify.notify import SendNotification
