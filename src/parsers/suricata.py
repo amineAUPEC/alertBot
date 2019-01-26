d6d78bcd20a5b9df4d35618332161b225ecb74cd
einar larsen <einarlarsen@outlook.com>
2019-01-26 17:15:53 +0100

    eve_json parser updated fast_log working

--- src/parsers/suricata.py
+++ src/parsers/suricata.py
@@ -3,14 +3,17 @@ import json
 import datetime
 import logging
 
+from src.notify import Notification
+
 logger = logging.getLogger("alertBot.suricata")
 
 
 class Suricata:
+    """ Suricata parser class """
 
-    def __init__(self):
+    def __init__(self, notify_enabled=False, dateformat: str = ""):
         # Copied from snort. Needs to be checked..
-        self.pattern = re.compile(
+        self._pattern_full = re.compile(
             r"(?P<time>\d+\/\d+\/\d+-\d+:\d+:\d+\.\d+)\s,"  # time
             r"(?P<gid>\d+),"  # Signature GID
             r"(?P<sid>\d+),"  # Signature SID
@@ -25,25 +28,44 @@ class Suricata:
             r"(?P<class>[a-zA-Z0-9-_ ]+),"  # Alert class
             r"(?P<pri>\d+)"  # Priority
         )
+        self._pattern_fast = re.compile(
+            r"(?P<time>\d{2}\/\d{2}\/\d{4}-\d{2}:\d{2}:\d{2}\.\d+)\s+"
+            r"\[\*\*\]\s+\[\d+:(?P<sid>\d+):(?P<revision>\d+)\] "
+            r"(?P<name>.+) \[\*\*\]\s+(\[Classification: (?P<classtype>.+)\] )?"
+            r"\[Priority: (?P<priority>\d+)\] \{(?P<protocol>[:a-zA-Z0-9_-]+)\} "
+            r"(?P<src>.+) \-\> (?P<dest>.+)"
+        )
+
+        # Output date format - see datetime for formatting options
+        self._dateformat = "%Y-%m-%d %H:%M:%S.%f"
+        if dateformat:
+            self._dateformat = dateformat
+
+        self.isNotify_enabled = notify_enabled
 
     def eve_json(self, line: str):
+        """ Eve.json 'parser' """
+
         alert = json.loads(line)
 
-        # since eve.json dont always contains alerts..
-        if alert["event_type"] != "alert":
-            # not an alert..
-            logger.debug("not an alert - event_type: %s", alert["event_type"])
+        # Since eve.json don't always contains alerts..
+        try:
+            if alert["event_type"] != "alert":
+                logger.debug("Event_type is not 'alert': %s", alert["event_type"])
+                return None
+        except KeyError:
+            # Some Suricata alerts don't have 'event_type'. Suricata trash that we dont care about any way..
+            logger.debug("Line did not contain 'event_type'. %s", line)
             return None
 
-        # try:
         new_alert = {
             "time": datetime.datetime.strptime(alert['timestamp'][:-5],
-                                               "%Y-%m-%dT%H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S"),
+                                               "%Y-%m-%dT%H:%M:%S.%f").strftime(self._dateformat),
             "name": alert['alert']['signature'],
             "src": alert['src_ip'],
             "src_port": alert['src_port'],
-            "dst": alert['dest_ip'],
-            "dst_port": alert['dest_port'],
+            "dest": alert['dest_ip'],
+            "dest_port": alert['dest_port'],
             "proto": alert['proto'],
             "action": alert["alert"]["action"],
             "payload": alert['payload'] if alert['payload'] else ""
@@ -52,50 +74,60 @@ class Suricata:
         # Request by alex.. thanks for messy code..
         try:
             new_alert["hostname"] = alert["http"]["hostname"]
-        except KeyError as ke:
+        except KeyError:
             logger.debug("Alert dont contain http hostname field..")
 
         try:
             new_alert["url"] = alert["http"]["url"]
-        except KeyError as ke:
+        except KeyError:
             logger.debug("Alert dont contain http url field..")
 
         try:
             new_alert["http_refer"] = alert["http"]["http_refer"]
-        except KeyError as ke:
+        except KeyError:
             logger.debug("Alert dont contain http http_refer field..")
 
         try:
             new_alert["http_method"] = alert["http"]["http_method"]
-        except KeyError as ke:
+        except KeyError:
             logger.debug("Alert dont contain http http_method field..")
 
         try:
             new_alert["http_user_agent"] = alert["http"]["http_user_agent"]
-        except KeyError as ke:
+        except KeyError:
             logger.debug("Alert dont contain http http_method field..")
 
         return new_alert
 
-        #except KeyError as ke:
-        #    logger.error(ke)
-        #    logger.debug(line)
-
     def full_log(self, line: str) -> dict:
-        # Parse snort version 2 alerts/logs
-
-        try:
-            match = self.pattern.match(line)
-            if not match:
-                # Send notification when nothing matches..
-                logger.error(f"No match for line. This should not happen! \n{line}")
-                exit(1)
-
-            return match.groupdict()
-
-        except re.error as rexerror:
-            logger.error(rexerror)
-            exit(1)
+        """ Parse Suricata 'full' logs """
+        return NotImplemented
 
     def fast_log(self, line: str) -> dict:
-        pass
+        """ Parse Suricata 'fast' logs """
+        # Not really tested...
+        try:
+            match = self._pattern_fast.match(line)
+            if not match:
+                # Send notification when nothing matches..
+                logger.error(f"No match for line. This should not happen! Line: %s", line)
+                if self.isNotify_enabled:
+                    Notification().send_notification(
+                        message="No match for line. This should not happen..\n{}".format(line),
+                        title="Snort Parser Error"
+                    )
+
+                return {}
+
+            parsed_alert = match.groupdict()
+
+            # Format time
+            formatted_time = datetime.datetime.strptime(parsed_alert["time"], '%m/%d/%Y-%H:%M:%S.%f')\
+                .strftime(self._dateformat)
+            parsed_alert["time"] = str(formatted_time)
+
+            return parsed_alert
+
+        except re.error as rex_error:
+            logger.error(rex_error)
+            exit(1)
