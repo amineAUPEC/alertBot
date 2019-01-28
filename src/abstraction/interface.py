d93ba50fe9e1e2c2159dc04214d74e3e5078da7c
einar larsen <einarlarsen@outlook.com>
2019-01-28 22:35:54 +0100

    Better doc strings

--- src/abstraction/interface.py
+++ src/abstraction/interface.py
@@ -3,10 +3,10 @@ from abc import ABCMeta, abstractmethod
 
 class IFaceNotify:
     __metaclass__ = ABCMeta
-    '''Interface for sending notifications'''
+    """ Interface for sending notifications that all notification agents must follow """
 
     @abstractmethod
     def send_alert(self, msg, title: str) -> bool:
-        ''' Send notification on grabbed nzb for a job '''
+        """ Send a notification abstractmethod """
         # msg should only be str or dict
         raise NotImplemented
