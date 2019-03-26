7f2011fee8b9104e067b35eefe238300fe252795
einar larsen <einarlarsen@outlook.com>
2019-03-26 23:12:15 +0100

    Removed logging/printing of the entire filter stats file during shutdown

--- alertBot.py
+++ alertBot.py
@@ -346,8 +346,7 @@ if __name__ == "__main__":
         # No reason to save current file state/poss since that prolly were errors occurs..
 
         if isFilter_enabled:
-            logger.info("Filter stats:")
-            logger.info(alert_filter.filter_stats())
+            logger.info("Check alert filter stats in 'filter_stats.json':")
             alert_filter.save_filter_stats()
 
         # Kill threads if any (ex file watcher).
