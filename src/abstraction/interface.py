b08ed06ee1821f1d86a50c4d11e56b6373ce05d1
einar larsen <einarlarsen@outlook.com>
2019-05-23 01:06:47 +0200

    Added IFace for sensors

--- src/abstraction/interface.py
+++ src/abstraction/interface.py
@@ -12,13 +12,18 @@ class IFaceNotify:
         raise NotImplemented
 
 
-class IFaceHTTPSource:
+class IFaceSensor:
+    """ Base class for all sensors """
     __metaclass__ = ABCMeta
-    """ Interface for sending notifications that all notification agents must follow """
-
     @abstractmethod
-    def __init__(self, *args, **kwargs):
+    def __init__(self, sensor_config, *args, **kwargs):
+        # No need to actual run super() on this one.. Just created to describe classes using this IFace
         raise NotImplemented
+
+
+class IFaceHTTPSource(IFaceSensor):
+    """ Interface all Sensors using HTTP as log source """
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def search(self, *args):
